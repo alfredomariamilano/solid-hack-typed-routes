6689a6017c7e342a4ca67cf5110fbbdf6d8878b9
Alfredo Maria Milano <alfmarmil@gmail.com>
2024-11-02 18:49:49 +0100

    fix: update Stackblitz demo link format and remove open-browser plugin from app configuration

--- app.config.ts
+++ app.config.ts
@@ -2,30 +2,14 @@ import path from 'node:path'
 import url from 'node:url'
 import { defineConfig } from '@solidjs/start/config'
 import { solidTypedRouterPlugin } from 'solid-typed-router'
-import type { Plugin } from 'vinxi'
 
 const root = import.meta.dirname || path.dirname(url.fileURLToPath(import.meta.url))
-let launchedBrowser = false
 
 export default defineConfig({
   vite({ router }) {
-    const openBrowserPlugin = {
-      name: 'open-browser',
-      configResolved(config) {
-        import('open').then(({ default: open }) => {
-          if (!launchedBrowser) {
-            open(`::${config.dev.port}`)
-
-            launchedBrowser = true
-          }
-        })
-      },
-    } as Plugin
-
     if (router !== 'server-function') {
       return {
         plugins: [
-          openBrowserPlugin,
           solidTypedRouterPlugin({
             root,
           }),
@@ -33,8 +17,6 @@ export default defineConfig({
       }
     }
 
-    return {
-      plugins: [openBrowserPlugin],
-    }
+    return {}
   },
 })
